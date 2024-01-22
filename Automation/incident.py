b9bfd2b6c3b7fbd76c8ecd5194e912633e8b0faa
George·Dong <georgedong32@foxmail.com>
2024-01-22 16:39:59 +0800

    Update CI

---
+++ Automation/incident.py
@@ -0,0 +1,35 @@
+import os
+import argparse
+import requests
+import json
+
+def UpdateIncident():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--token', type=str, required=True)
+    parser.add_argument('--url', type=str, required=True)
+    parser.add_argument('--title', type=str, required=True)
+    parser.add_argument('--content', type=str, required=True)
+    parser.add_argument('--status', type=str, required=True)
+    args = parser.parse_args()
+    url = args.url
+    token = args.token
+    body = args.content
+    body = body.replace('[]', '[&#96;commit-hash&#96;]')
+    tempstr = body.split('commit/')[1]
+    tempstr = tempstr[0:7]
+    body = body.replace('commit-hash', tempstr)
+    headers = {
+        "Authorization": f"Bearer {token}",  # 使用你的Token
+        "Content-Type": "application/json"
+    }
+    data={
+        "title": args.title,
+        "body": body,
+        "status": args.status,
+    }
+    response = requests.post(url, headers=headers, json=data)
+
+    return response.json()
+
+if __name__ == '__main__':
+    UpdateIncident()
